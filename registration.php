d4fef63da78f02b2df90c70e59371a0071c5e21c
chienphamk58 <phamdinhchienk58@gmail.com>
2018-01-17 15:43:40 +0700

    Init HelloWorld Module

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pdc
+ * Date: 17/01/2018
+ * Time: 13:52
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Magestore_HelloWorld',
+    __DIR__
+);
